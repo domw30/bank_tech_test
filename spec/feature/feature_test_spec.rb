270ff09899e2a020252927eb563f301d18679937
domw30 <domw30@gmail.com>
2019-06-04 15:11:26 +0100

    add feature test folder/file, add user stories

---
+++ spec/feature/feature_test_spec.rb
@@ -0,0 +1,30 @@
+require 'bank'
+require 'transactions'
+require 'print_statement'
+
+describe 'User Stories' do
+
+# As a user,
+# So that I can keep track of my money,
+# I'd like to be able to create a new bank account
+# with a default balance of zero.
+
+# As a user,
+# So that I can store money safely,
+# I'd like to be able to deposit my
+# money into my bank account.
+
+# As a user,
+# So that I can retrieve my stored money,
+# I'd like to be able to withdraw my money
+# out of my bank account.
+
+# As a user,
+# So that I can remember my bank history,
+# I'd like to be able to print a bank statement.
+
+# As a user,
+# So that I can recall the timing of my transactions,
+# I'd like to be able to view the date of transactions.
+
+end
