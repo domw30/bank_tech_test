16001dc4d56416aeadedebff96ad064d43950695
domw30 <domw30@gmail.com>
2019-06-04 10:43:29 +0100

    add first bank test to increase coverage, add double

--- spec/bank_spec.rb
+++ spec/bank_spec.rb
@@ -2,4 +2,17 @@ require 'bank'
 
 describe Bank do
 
+  subject(:bank) { Bank.new(transactions) }
+  let(:transactions) { double("transactions") }
+
+
+  context 'so that the methods can be called from transactions' do
+
+    describe '#transactions' do
+      it 'should create an instance of transactions' do
+        expect(bank.transactions).to eq transactions
+      end
+    end
+
+  end
 end
